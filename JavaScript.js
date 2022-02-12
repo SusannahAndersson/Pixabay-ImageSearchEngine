e3140458fbc4a00f84bc895f1e6944b6606a9bdd
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-02-12 10:36:33 +0100

    add pagination

--- JavaScript.js
+++ JavaScript.js
@@ -1,33 +1,64 @@
-﻿
+﻿let currentPage = 1;
+let lastSearchString = "";
+let lastSearchColor = "";
+let totalHits = 0;
+
+let template;
+setUpTemplate();
+function setUpTemplate() {
+    template = document.querySelector('#hit-template');
+    template.remove();
+}
+
 formHandler();
 
 function formHandler(){
     let searchForm = document.querySelector('.InputSearch');
     let searchString = document.querySelector('#Search');
     let color = document.querySelector('#color');
-    // let movies = document.querySelector('.movies');
-
+    
     searchForm.onsubmit = event => {
+        lastSearchColor = color.value; // cache
+        lastSearchString = searchString.value; // cache
         event.preventDefault();
-        alert("Search: " + searchString.value + " Color: " + color.value);
+        searchPixaby(searchString.value, color.value, 1);
     }
 }
 
-start();
+nextPageButtonHandler();
+function nextPageButtonHandler() {
+    let button = document.querySelector('#next');
+    button.onclick = () => {
+        changePage(++currentPage);
+    }
+}
 
-// TODO rename and move to search
-async function start() {
-    let pixabay = await getPixabayData('rose', 'blue');
+previousPageButtonHandler();
+function previousPageButtonHandler() {
+    let button = document.querySelector('#previous');
+    button.onclick = () => {
+        changePage(--currentPage);
+    }
+}
 
-    // let totalHitsSpan = document.querySelector('#total-hits');
-    // totalHitsSpan.textContent = pixabay.totalHits;
- 
+function changePage(newPageNumber){
+    currentPage = newPageNumber; // cache
+    removeCurrentSearchItems();
+    searchPixaby(lastSearchString, lastSearchColor, newPageNumber);
+}
+
+// Keep track of total amount
+// TODO stop functionality for next and previous button when no more hits
+
+async function searchPixaby(searchQuery, searchColor, pageNumber) {
+    let pixabay = await getPixabayData(searchQuery, searchColor, pageNumber);
+    removeCurrentSearchItems();
+    totalHits = pixabay.totalHits; // cache
+
     let hitList = document.querySelector('#hit-list');
-    let template = document.querySelector('#hit-template');
-    template.remove(); // remove the template from the html document
     
     for (const image of pixabay.hits) {
-        let li = template.content.firstElementChild.cloneNode(true); // true = copy all children
+        let li = template.content.firstElementChild.cloneNode(true);
         li.querySelector('img').src = image.previewURL;
         li.querySelector('#tags').textContent = image.tags;
         li.querySelector('#photographer').textContent = image.user;
@@ -35,12 +66,12 @@ async function start() {
     }
 }
 
-// TODO set in parameters for function
-async function getPixabayData(searchQuery, searchColor) {
+async function getPixabayData(searchQuery, searchColor, pageNumber) {
     let params = new URLSearchParams({
         key : '25655500-534d4ee5283250b244508c508',
         q : searchQuery,
         colors : searchColor,
+        page : pageNumber,
         per_page : '10'
     });
     
@@ -49,6 +80,13 @@ async function getPixabayData(searchQuery, searchColor) {
     return json;
 }
 
+function removeCurrentSearchItems() {
+    let hitList = document.querySelector('#hit-list');
+    let children = hitList.querySelectorAll('li');
+    for (const child of children) {
+        child.remove();
+    }
+}
 
 
 
